08b5527a9d07d411ce86b1f7965575eaae04884a
samandaji <contactabubakarusman@gmail.com>
2023-12-09 14:29:47 +0100

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log(document.getElementById("myContainer"));
+
+function myFunction() {
+	let name = document.getElementById("search").value;
+	let message = "<h2> Hello" + name "!</h2>";
+}
